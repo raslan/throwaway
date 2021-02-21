c1de784c5385d34c0216a722755c7efdfc2c807d
Ali Raslan <24810123+aliraslan@users.noreply.github.com>
2021-02-21 21:09:20 +0200

    feat: wrap extension in Chakra container

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import { ChakraProvider, Box, Container } from "@chakra-ui/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+
+function App({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <Box
+        bg='teal.900'
+        w='30vw'
+        minW='400px'
+        maxW='600px'
+        h='80vh'
+        minH='xl'
+        maxH='2xl'
+        p={4}
+        color='white'
+      >
+        <Container h='full' w='full' p='6' centerContent>
+          <Component {...pageProps} />
+        </Container>
+      </Box>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default App;
