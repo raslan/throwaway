2b0f457e3097db18e8b28dbfbc603081540bd2be
Ali Raslan <24810123+aliraslan@users.noreply.github.com>
2021-02-22 00:15:57 +0200

    feat: add first email iteration

---
+++ pages/email.js
@@ -0,0 +1,219 @@
+import React, { useState, useEffect } from "react";
+import sanitizeHtml from "sanitize-html";
+
+import {
+  Box,
+  Button,
+  Text,
+  SlideFade,
+  Flex,
+  SkeletonText,
+  SkeletonCircle,
+} from "@chakra-ui/react";
+
+import { v4 } from "uuid";
+import axios from "axios";
+import Link from "next/link";
+import { ArrowBackIcon, EmailIcon, RepeatIcon } from "@chakra-ui/icons";
+
+const generateEmail = () => {
+  const g = `${v4().split`-`[0]}${Date.now()}`;
+  window?.localStorage?.setItem("throwaway_email", JSON.stringify(g));
+  return g;
+};
+
+const populateEmail = () => {
+  let found = JSON.parse(window?.localStorage?.getItem("throwaway_email"));
+  return found ? found : generateEmail();
+};
+
+const read = async (id) =>
+  await (
+    await axios.get(
+      `https://www.1secmail.com/api/v1/?action=getMessages&login=${id}&domain=wwjmp.com`
+    )
+  ).data;
+
+const readOne = async (id, selected) =>
+  await (
+    await axios.get(
+      `https://www.1secmail.com/api/v1/?action=readMessage&login=${id}&domain=wwjmp.com&id=${selected}`
+    )
+  ).data;
+
+const SingleEmail = (props) => {
+  const [content, setContent] = useState({});
+  useEffect(async () => {
+    setContent(await readOne(props.id, props.selected));
+  }, []);
+  return (
+    <>
+      <Flex alignItems='center' my='3' w='full'>
+        <ArrowBackIcon
+          w={6}
+          h={6}
+          cursor='pointer'
+          onClick={() => props.setMode("inbox")}
+        />
+      </Flex>
+      <SkeletonText
+        w='full'
+        h='full'
+        noOfLines={8}
+        spacing='8'
+        mt='4'
+        isLoaded={content.body}
+        overflow='scroll'
+      >
+        <SlideFade in={true}>
+          <Box h='full'>
+            <Text fontSize='lg'>from: {content?.from}</Text>
+            <Text fontWeight='semibold'>subject: {content?.subject}</Text>
+            <Box
+              mt='5'
+              dangerouslySetInnerHTML={{ __html: sanitizeHtml(content?.body) }}
+            />
+          </Box>
+        </SlideFade>
+      </SkeletonText>
+    </>
+  );
+};
+
+const Email = () => {
+  const [id, setId] = useState("");
+  const [emails, setEmails] = useState([]);
+  const [selected, setSelected] = useState("");
+  const [mode, setMode] = useState("inbox");
+
+  useEffect(async () => {
+    setId(populateEmail());
+    setEmails(await read(id));
+  }, []);
+  return (
+    <>
+      {mode === "inbox" && (
+        <>
+          <Box w='full'>
+            <Flex alignItems='center' my='3'>
+              <Link href='/'>
+                <ArrowBackIcon w={6} h={6} cursor='pointer' />
+              </Link>
+
+              <Text mx='8' textAlign='center' fontSize='xl' fontWeight='bold'>
+                Email
+              </Text>
+            </Flex>
+            <Box
+              fontWeight='bold'
+              fontSize='sm'
+              textAlign='center'
+              bg='blackAlpha.600'
+              p='3'
+              rounded='lg'
+              w='full'
+              my='3.5'
+              mt='7'
+              onClick={(e) => {
+                navigator.clipboard.writeText(id + "@wwjmp.com");
+                const range = document.createRange();
+                range.selectNodeContents(e.target);
+                window.getSelection().removeAllRanges();
+                window.getSelection().addRange(range);
+              }}
+            >
+              {id}@wwjmp.com
+            </Box>
+            <Button
+              onClick={async () => {
+                setId(generateEmail());
+                setEmails(await read(id));
+              }}
+              colorScheme='blackAlpha'
+              w='full'
+            >
+              Generate A New Email
+            </Button>
+          </Box>
+          <Box
+            w='full'
+            mt='12'
+            textAlign='center'
+            display='flex'
+            justifyContent='space-between'
+          >
+            <Text fontSize='lg' fontWeight='bold' alignItems='center'>
+              Inbox
+            </Text>
+            <RepeatIcon
+              cursor='pointer'
+              w={6}
+              h={6}
+              onClick={async () => setEmails(await read(id))}
+            />
+          </Box>
+          <Box w='full' overflow='scroll' rounded='lg' mt='3'>
+            <Box w='full'>
+              {emails.length === 0 && (
+                <>
+                  {Array(3)
+                    .fill(1)
+                    .map((_) => (
+                      <>
+                        <SkeletonCircle
+                          size='5'
+                          isLoaded={emails.length > 0}
+                          my='3'
+                        />
+                        <SkeletonText
+                          noOfLines={3}
+                          spacing='4'
+                          isLoaded={emails.length > 0}
+                        />
+                      </>
+                    ))}
+                </>
+              )}
+              {emails.map((email, index) => (
+                <SlideFade in={true}>
+                  <Box
+                    id={`email-${index}`}
+                    key={index}
+                    w='full'
+                    p='6'
+                    my='2'
+                    bgColor='blackAlpha.600'
+                    rounded='lg'
+                    cursor='pointer'
+                    onClick={() => {
+                      setMode("email");
+                      setSelected(email.id);
+                    }}
+                    display='flex'
+                    alignItems='center'
+                  >
+                    <EmailIcon w={6} h={6} />
+                    <Flex direction='column' ml='6'>
+                      <Text fontWeight='bold'>{email.from}</Text>
+                      <Text fontSize='lg' mt='1'>
+                        {email.subject}
+                      </Text>
+                      <Text fontSize='sm' mt='1'>
+                        {email.date}
+                      </Text>
+                    </Flex>
+                  </Box>
+                </SlideFade>
+              ))}
+            </Box>
+          </Box>
+        </>
+      )}
+      {mode === "email" && (
+        <SingleEmail selected={selected} id={id} setMode={setMode} />
+      )}
+    </>
+  );
+};
+
+export default Email;
