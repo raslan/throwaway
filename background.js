57d06ac481f05de405d38000ccc211fed5f2afff
Ali Raslan <24810123+raslan@users.noreply.github.com>
2021-04-27 04:51:29 +0200

    feat: Add background script.

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.runtime.onMessage.addListener((message) => {
+  chrome.tabs.query({}, (tabs) => {
+    tabs.forEach((tab) => {
+      if (tab.id) {
+        chrome.tabs.sendMessage(tab.id, message);
+      }
+    });
+  });
+});
