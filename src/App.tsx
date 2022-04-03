04a66833765992352d49e004029ff4d7c80033e4
raslan <24810123+raslan@users.noreply.github.com>
2022-04-03 04:38:15 +0200

    feat: add more generic app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import { faker } from "@faker-js/faker";
+import { useLocalStorage } from "usehooks-ts";
+import { View } from "src/types";
+import IdentityView from "./views/IdentityView";
+import MainView from "./views/MainView";
+import SettingsView from "./views/SettingsView";
+import Sidebar from "./components/Sidebar";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  const fill = (message: any) => chrome?.runtime?.sendMessage(message);
+  const [view] = useLocalStorage<View>("throwaway-view", View.Main);
   return (
-    <div className='bg-slate-900 w-[640px] h-[400px] text-white flex flex-col'>
-      <button
-        onClick={() => fill(faker.helpers.contextualCard())}
-        className='px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80'
-      >
-        Fill Form
-      </button>
+    <div className='bg-slate-900 w-[640px] h-[400px] text-white flex flex-col overflow-hidden'>
+      <div className='flex w-full'>
+        <div className='w-max'>
+          <Sidebar />
+        </div>
+        {view === View.Main && <MainView />}
+        {view === View.Identity && <IdentityView />}
+        {view === View.Settings && <SettingsView />}
+        <Toaster position='top-right' />
+      </div>
     </div>
   );
 }
