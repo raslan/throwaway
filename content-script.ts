202ffb7c73a7ba20eca74c9263432598d812157f
raslan <24810123+raslan@users.noreply.github.com>
2022-04-15 00:07:37 +0200

    feat: make content script more flexible

--- content-script.ts
+++ content-script.ts
@@ -7,19 +7,35 @@ chrome.runtime.onMessage.addListener((state) => {
   const inputs = [...document.querySelectorAll("input")];
   // Map searchable properties in Fuse
   const fuse = new Fuse(inputs, {
-    keys: ["name", "class", "id", "type", "placeholder"],
+    keys: ["name", "class", "id", "type", "placeholder", "aria-label"],
+    // Add settings to adjust these
     minMatchCharLength: 4,
-    findAllMatches: true,
-    distance: 0,
-    threshold: 0.4,
+    distance: 4,
+    threshold: 0.32,
+    ignoreLocation: true,
   });
+
+  [...document.querySelectorAll("select")].forEach((select) => {
+    select.selectedIndex = Math.floor(Math.random() * select.length);
+    const newEvent = new Event("change", { bubbles: true });
+    select.dispatchEvent(newEvent);
+  });
+
+  inputs.forEach((input: any) => {
+    if (input.type === "radio" || input.type === "checkbox") {
+      input.click();
+    }
+  });
+
   // Search for each property given by the frontend and fill any matching input fields
   Object.keys(state).forEach((key) => {
     fuse.search(key).forEach(({ item: input }) => {
-      input.value = state.email;
+      input.value = state[key];
       const newEvent = new Event("input", { bubbles: true });
       input.dispatchEvent(newEvent);
     });
   });
   return true;
 });
+
+export default {};
