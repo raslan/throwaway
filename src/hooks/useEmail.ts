a5ed23da1865f2a8b44c3e8c0d9b49dfc74fc756
raslan <24810123+raslan@users.noreply.github.com>
2022-04-15 00:09:37 +0200

    feat: create useEmail hook

---
+++ src/hooks/useEmail.ts
@@ -0,0 +1,64 @@
+import { useInterval } from "@refolded/use-timing";
+import { differenceInHours } from "date-fns";
+import { useCallback, useEffect, useState } from "react";
+import { Email } from "src/types";
+import { useLocalStorage } from "usehooks-ts";
+import useFetch from "./useFetch";
+
+const eFetch = (url: string) => fetch(url).then((res) => res.json());
+
+const useEmail = () => {
+  const [lastUpdated, setLastUpdated] = useLocalStorage<Date>(
+    "throwaway-email-lastupdate",
+    new Date()
+  );
+  const [email, setEmail] = useLocalStorage("throwaway-email", "");
+  const [emails, setMail] = useState<Email[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  //   Utility functions to generate new email and read an inbox
+  const getNewEmail = useCallback(() => {
+    eFetch(`${import.meta.env.VITE_API_URL}`).then((data) => {
+      setEmail(data.email);
+    });
+  }, []);
+
+  const { data, error, refresh } = useFetch<any>(
+    `${import.meta.env.VITE_API_URL}${email}`
+  );
+
+  useEffect(() => {
+    if (!data && !error) {
+      setLoading(true);
+    } else {
+      setLoading(false);
+    }
+
+    if (data?.emails) {
+      setMail(data.emails);
+    } else if (error && differenceInHours(lastUpdated, new Date()) > 1) {
+      getNewEmail();
+      setLastUpdated(new Date());
+    }
+  }, [data, error]);
+
+  useEffect(() => {
+    if (!email) {
+      getNewEmail();
+    }
+  }, []);
+
+  //   Periodically update the mail
+  const { start: watch } = useInterval(() => {
+    refresh();
+  }, 10000);
+
+  //   Initially make sure the email is still valid and fetch mail
+  useEffect(() => {
+    watch();
+  }, []);
+
+  return { email, emails, loading, getNewEmail };
+};
+
+export default useEmail;
