124381f2e6cc414d3a6e9ad5ae308d4edbabcd19
raslan <24810123+raslan@users.noreply.github.com>
2022-04-14 17:32:17 +0200

    feat: only invoke content script on active tab

--- service-worker.ts
+++ service-worker.ts
@@ -1,15 +1,22 @@
 /// <reference types="chrome"/>
 // Listen for messages from the frontend and call autofill.
 chrome.runtime.onMessage.addListener((message) => {
-  chrome.tabs.query({}, (tabs) => {
-    tabs.forEach((tab) => {
-      if (tab.id) {
-        chrome.scripting.executeScript({
-          target: { tabId: tab.id },
-          files: ["content-script.js"],
-        });
-        chrome.tabs.sendMessage(tab.id, message);
-      }
-    });
-  });
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      tabs.forEach((tab) => {
+        if (tab.id) {
+          chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ["content-script.ts.js"],
+          });
+          chrome.tabs.sendMessage(tab.id, message);
+        }
+      });
+    }
+  );
+  return true;
 });
