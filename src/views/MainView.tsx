b41917605bd5f079dee122b8e4920f32ecb1d371
raslan <24810123+raslan@users.noreply.github.com>
2022-04-15 00:10:13 +0200

    feat: add email support

--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -1,48 +1,105 @@
-import faker from "@faker-js/faker";
 import Fuse from "fuse.js";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import EmailDialog from "src/components/EmailDialog";
 import EmailPreview from "src/components/EmailPreview";
+import InboxIcon from "src/components/InboxIcon";
 import SearchBar from "src/components/SearchBar";
+import SpinnerIcon from "src/components/SpinnerIcon";
+import useEmail from "src/hooks/useEmail";
 import { Email } from "src/types";
-
-const emails = Array(100)
-  .fill(1)
-  .map((_) => ({
-    subject: `${faker.hacker.phrase()} ${faker.hacker.phrase()}`,
-    from: faker.hacker.noun(),
-    body_text: faker.hacker.noun(),
-    body_html: faker.hacker.ingverb(),
-  }));
-
-const fuse = new Fuse(emails, {
-  keys: ["subject", "from"],
-  minMatchCharLength: 3,
-});
+import toast from "react-hot-toast";
+import { useCopyToClipboard } from "usehooks-ts";
 
 const MainView = () => {
+  const { email, emails, loading, getNewEmail } = useEmail();
+  const [isOpen, setIsOpen] = useState(false);
+  const [emailData, setEmailData] = useState<Email>();
+  const [, copy] = useCopyToClipboard();
+
+  const fuse = useMemo(
+    () =>
+      new Fuse(emails, {
+        keys: ["subject", "from"],
+        minMatchCharLength: 4,
+        distance: 4,
+        threshold: 0.32,
+        ignoreLocation: true,
+      }),
+    [emails]
+  );
+
   const [search, setSearch] = useState("");
   const [filteredEmails, setFilteredEmails] = useState<Email[]>(emails);
 
   useEffect(() => {
+    setFilteredEmails(emails);
+  }, [emails]);
+
+  useEffect(() => {
     if (search)
-      setFilteredEmails(
-        fuse
-          .search(search)
-          .map(({ item }) => item)
-          .reverse() as Email[]
-      );
-    else setFilteredEmails(emails.reverse());
+      setFilteredEmails(fuse.search(search).map(({ item }) => item) as Email[]);
+    else setFilteredEmails(emails);
   }, [search]);
 
   return (
     <div className='flex flex-col w-full'>
       <section className='w-full max-w-md px-5 py-4 mx-auto rounded-md'>
-        <SearchBar search={search} setSearch={setSearch} />
-        <div className='flex flex-wrap h-[20rem] max-h-full overflow-y-scroll'>
+        <div className='grid grid-cols-12 w-full justify-between items-center gap-4'>
+          <SearchBar
+            className='col-span-10'
+            search={search}
+            setSearch={setSearch}
+          />
+          <button
+            onClick={() => {
+              getNewEmail();
+              toast.success("Created a new email!");
+            }}
+          >
+            <InboxIcon />
+          </button>
+          {loading && <SpinnerIcon />}
+        </div>
+        <div className='flex flex-wrap h-[20rem] max-h-full overflow-y-scroll content-start mt-4 pb-5'>
+          {!loading && filteredEmails.length === 0 && (
+            <div className='flex flex-col w-full h-3/4 items-center justify-center'>
+              <div>
+                <h1 className='text-3xl text-gray-400'>Inbox is empty</h1>
+                <div className='mt-3'>
+                  <SpinnerIcon />
+                </div>
+              </div>
+              <p className='text-gray-400 mt-3'>
+                Your email is{" "}
+                <button
+                  onClick={() => {
+                    copy(email);
+                    toast.success("Copied to clipboard");
+                  }}
+                  className='text-purple-300 hover:underline'
+                >
+                  {email}
+                </button>
+              </p>
+            </div>
+          )}
           {filteredEmails.map((email, i) => (
-            <EmailPreview subject={email.subject} from={email.from} key={i} />
+            <EmailPreview
+              subject={email.subject}
+              from={email.from}
+              key={i}
+              onClick={() => {
+                setEmailData(email);
+                setIsOpen(true);
+              }}
+            />
           ))}
         </div>
+        <EmailDialog
+          emailData={emailData as Email}
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+        />
       </section>
     </div>
   );
