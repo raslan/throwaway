bd2c5adf8995401100bbdb8aab23becf3887314e
raslan <24810123+raslan@users.noreply.github.com>
2022-04-22 01:53:41 +0200

    feat: add typescript declaration for parse-otp

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+declare module "parse-otp-message" {
+  export default function parseOtpMessage(
+    message: string,
+    opts?: {
+      isGlobal?: boolean;
+      cleanCode?: (code: string) => string;
+    }
+  ): {
+    code: string | undefined;
+  };
+}
