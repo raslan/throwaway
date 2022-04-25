aaf04aeb7f4a55a5f2fbc7ac0bdf8d5a241ec9c9
raslan <24810123+raslan@users.noreply.github.com>
2022-04-26 00:11:05 +0200

    feat: initialize settings view

--- src/views/SettingsView.tsx
+++ src/views/SettingsView.tsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import Dropdown from "src/components/Dropdown";
+
 const SettingsView = () => {
-  return <div>SettingsView</div>;
+  const [settings, setSettings] = useState([
+    {
+      name: "Debug Mode",
+      options: [true, false],
+      value: false,
+    },
+    {
+      name: "Theme",
+      options: ["dark", "light"],
+      value: false,
+    },
+  ]);
+  return (
+    <div className='flex flex-col gap-3 w-full p-8 px-12'>
+      {settings.map((setting) => (
+        <Dropdown
+          setting={setting}
+          setSelected={(value) => {
+            const newSettings = [...settings];
+            newSettings[newSettings.indexOf(setting)].value = value;
+            setSettings(newSettings);
+          }}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default SettingsView;
