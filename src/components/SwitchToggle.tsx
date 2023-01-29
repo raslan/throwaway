c7dce7eb41981bbebb945d817fbe4c07e3db1df3
raslan <24810123+raslan@users.noreply.github.com>
2023-01-29 16:41:17 +0200

    feat: modify toggle styling

--- src/components/SwitchToggle.tsx
+++ src/components/SwitchToggle.tsx
@@ -22,21 +22,25 @@ export default function SwitchToggle({
   return (
     <div className='flex flex-col gap-2 justify-center w-full'>
       <Switch.Group>
-        <Switch.Label className='font-bold text-sm'>{label}</Switch.Label>
-        <Switch.Label className='text-xs'>{description}</Switch.Label>
-        <Switch
-          checked={enabled}
-          onChange={setEnabled}
-          className={`${enabled ? 'bg-purple-600' : 'bg-gray-500'}
+        <div className='flex justify-between pr-3'>
+          <Switch.Label className='font-bold text-sm'>{label}</Switch.Label>
+          <Switch
+            checked={enabled}
+            onChange={setEnabled}
+            className={`${enabled ? 'bg-purple-600' : 'bg-gray-500'}
     relative inline-flex h-[20px] w-[45px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
-        >
-          <span className='sr-only'>Use setting</span>
-          <span
-            aria-hidden='true'
-            className={`${enabled ? 'translate-x-6' : 'translate-x-0'}
+          >
+            <span className='sr-only'>Use setting</span>
+            <span
+              aria-hidden='true'
+              className={`${enabled ? 'translate-x-6' : 'translate-x-0'}
       pointer-events-none inline-block h-[16px] w-[16px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
-          />
-        </Switch>
+            />
+          </Switch>
+        </div>
+        <Switch.Label className='text-xs text-gray-300'>
+          {description}
+        </Switch.Label>
       </Switch.Group>
     </div>
   );
