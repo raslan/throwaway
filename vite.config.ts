1bd7d33b806d191e2466d87ee4321a1978a33efe
raslan <24810123+raslan@users.noreply.github.com>
2023-01-29 12:21:51 +0200

    feat: use new crx extension and swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { chromeExtension } from "rollup-plugin-chrome-extension";
-import tsconfigPaths from "vite-tsconfig-paths";
-import * as manifest from "./manifest.json";
-import zip from "rollup-plugin-zip";
-import copy from "rollup-plugin-copy2";
+import { defineConfig, Plugin } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { crx } from '@crxjs/vite-plugin';
+import manifest from './manifest.json';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import zip from 'rollup-plugin-zip';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
     react(),
-    chromeExtension({ manifest }),
-    copy({
-      assets: [
-        ["public/assets/icon-32.png", "assets/icon-32.png"],
-        ["public/assets/icon-128.png", "assets/icon-128.png"],
-        ["public/assets/icon-192.png", "assets/icon-192.png"],
-      ],
-    }),
-    zip(),
+    crx({ manifest }),
+    zip() as unknown as Plugin,
   ],
 });
