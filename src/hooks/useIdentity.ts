225728d6b878d4d500de8c77d8cf28dbef35cecf
raslan <24810123+raslan@users.noreply.github.com>
2023-01-29 12:27:20 +0200

    refactor: rebuild useIdentity for v3

--- src/hooks/useIdentity.ts
+++ src/hooks/useIdentity.ts
@@ -1,31 +1,16 @@
-import { faker } from "@faker-js/faker";
-import { useCallback, useEffect } from "react";
-import { useLocalStorage } from "usehooks-ts";
-import useEmail from "./useEmail";
-import { new_card } from "src/lib/generate_card.js";
-
-const flatten = (obj: Record<string, any>) => {
-  const result: Record<string, any> = {};
-  for (const [key, value] of Object.entries(obj)) {
-    if (typeof value === "object") {
-      const flat = flatten(value);
-      for (const [subKey, subValue] of Object.entries(flat)) {
-        result[`${key}.${subKey}`] = subValue;
-      }
-    } else {
-      result[key] = value;
-    }
-  }
-  return result;
-};
+import { faker } from '@faker-js/faker';
+import { useCallback, useEffect } from 'react';
+import { useLocalStorage } from 'usehooks-ts';
+import useEmail from './useEmail';
+import { new_card } from 'src/lib/generate_card.js';
 
 const useIdentity = (): {
   identity: Record<string, any>;
   newIdentity: () => void;
 } => {
-  const { email, otp } = useEmail();
+  const { email, otp, getNewEmail } = useEmail();
   const [identity, setIdentity] = useLocalStorage<Record<string, any>>(
-    "throwaway-identity",
+    'throwaway-identity',
     {}
   );
 
@@ -35,25 +20,25 @@ const useIdentity = (): {
     const card_expiry = `0${Math.floor(Math.random() * 8) + 1}/${
       Math.floor(Math.random() * 7) + 22
     }`;
+    const newEmail = getNewEmail();
     const card_number = new_card();
     const generatedIdentity = {
-      ...flatten(faker.helpers.contextualCard()),
-      email,
+      email: newEmail,
       name,
       avatar: `https://api.lorem.space/image?w=500&h=500&x=${
         Math.floor(Math.random() * 899) + 100
       }`,
       card_number,
-      "cc-number": card_number,
+      'cc-number': card_number,
       cardNumber: card_number,
       card_expiry,
-      "cc-exp": card_expiry,
+      'cc-exp': card_expiry,
       cvc,
       card_verification: cvc,
-      "cc-csc": cvc,
+      'cc-csc': cvc,
       job_title: faker.name.jobTitle(),
-      first_name: name.split(" ")[0],
-      last_name: name.split(" ")[1],
+      first_name: name.split(' ')[0],
+      last_name: name.split(' ')[1],
       state: faker.address.state(),
       city: faker.address.city(),
       street_address: faker.address.streetAddress(),
@@ -66,14 +51,14 @@ const useIdentity = (): {
       password: faker.internet.password() + faker.internet.password(),
       suite: faker.address.secondaryAddress(),
       apartment: faker.address.secondaryAddress(),
-      dateofbirth: faker.date.past(50).toISOString().split("T")[0],
-      date: faker.date.past(50).toISOString().split("T")[0],
+      dateofbirth: faker.date.past(50).toISOString().split('T')[0],
+      date: faker.date.past(50).toISOString().split('T')[0],
       otp,
       code: otp,
       verification_code: otp,
     };
     setIdentity(generatedIdentity);
-  }, [email, otp]);
+  }, [otp]);
 
   useEffect(() => {
     if (!identity.name) {
@@ -87,10 +72,10 @@ const useIdentity = (): {
     chrome.storage.local.set({ identity: JSON.stringify(identity) });
     chrome.storage.local.set({
       throwaway_env: JSON.stringify({
-        VITE_API_URL: import.meta.env.VITE_API_URL || "",
+        VITE_API_URL: import.meta.env.VITE_API_URL || '',
       }),
     });
-  }, [identity]);
+  }, [identity, email]);
 
   return {
     identity,
