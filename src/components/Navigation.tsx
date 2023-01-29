b2c325d7bc772f229d6a1212d7932033f0a6480a
raslan <24810123+raslan@users.noreply.github.com>
2023-01-29 12:26:26 +0200

    feat: add navigation component

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,97 @@
+import { useLocalStorage } from 'usehooks-ts';
+
+const options = [
+  {
+    label: 'Identity',
+    icon: () => (
+      <svg
+        width={24}
+        height={24}
+        viewBox='0 0 24 24'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path
+          fillRule='evenodd'
+          clipRule='evenodd'
+          d='M16 9C16 11.2091 14.2091 13 12 13C9.79086 13 8 11.2091 8 9C8 6.79086 9.79086 5 12 5C14.2091 5 16 6.79086 16 9ZM14 9C14 10.1046 13.1046 11 12 11C10.8954 11 10 10.1046 10 9C10 7.89543 10.8954 7 12 7C13.1046 7 14 7.89543 14 9Z'
+          fill='currentColor'
+        />
+        <path
+          fillRule='evenodd'
+          clipRule='evenodd'
+          d='M12 1C5.92487 1 1 5.92487 1 12C1 18.0751 5.92487 23 12 23C18.0751 23 23 18.0751 23 12C23 5.92487 18.0751 1 12 1ZM3 12C3 14.0902 3.71255 16.014 4.90798 17.5417C6.55245 15.3889 9.14627 14 12.0645 14C14.9448 14 17.5092 15.3531 19.1565 17.4583C20.313 15.9443 21 14.0524 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12ZM12 21C9.84977 21 7.87565 20.2459 6.32767 18.9878C7.59352 17.1812 9.69106 16 12.0645 16C14.4084 16 16.4833 17.1521 17.7538 18.9209C16.1939 20.2191 14.1881 21 12 21Z'
+          fill='currentColor'
+        />
+      </svg>
+    ),
+    path: 'identity',
+  },
+  {
+    label: 'Email',
+    icon: () => (
+      <svg
+        width={24}
+        height={24}
+        viewBox='0 0 24 24'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path
+          fillRule='evenodd'
+          clipRule='evenodd'
+          d='M3.00977 5.83789C3.00977 5.28561 3.45748 4.83789 4.00977 4.83789H20C20.5523 4.83789 21 5.28561 21 5.83789V17.1621C21 18.2667 20.1046 19.1621 19 19.1621H5C3.89543 19.1621 3 18.2667 3 17.1621V6.16211C3 6.11449 3.00333 6.06765 3.00977 6.0218V5.83789ZM5 8.06165V17.1621H19V8.06199L14.1215 12.9405C12.9499 14.1121 11.0504 14.1121 9.87885 12.9405L5 8.06165ZM6.57232 6.80554H17.428L12.7073 11.5263C12.3168 11.9168 11.6836 11.9168 11.2931 11.5263L6.57232 6.80554Z'
+          fill='currentColor'
+        />
+      </svg>
+    ),
+    path: 'email',
+  },
+  {
+    label: 'Settings',
+    icon: () => (
+      <svg
+        width={24}
+        height={24}
+        viewBox='0 0 24 24'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path
+          fillRule='evenodd'
+          clipRule='evenodd'
+          d='M7 3C8.86384 3 10.4299 4.27477 10.874 6H19V8H10.874C10.4299 9.72523 8.86384 11 7 11C4.79086 11 3 9.20914 3 7C3 4.79086 4.79086 3 7 3ZM7 9C8.10457 9 9 8.10457 9 7C9 5.89543 8.10457 5 7 5C5.89543 5 5 5.89543 5 7C5 8.10457 5.89543 9 7 9Z'
+          fill='currentColor'
+        />
+        <path
+          fillRule='evenodd'
+          clipRule='evenodd'
+          d='M17 20C15.1362 20 13.5701 18.7252 13.126 17H5V15H13.126C13.5701 13.2748 15.1362 12 17 12C19.2091 12 21 13.7909 21 16C21 18.2091 19.2091 20 17 20ZM17 18C18.1046 18 19 17.1046 19 16C19 14.8954 18.1046 14 17 14C15.8954 14 15 14.8954 15 16C15 17.1046 15.8954 18 17 18Z'
+          fill='currentColor'
+        />
+      </svg>
+    ),
+    path: 'settings',
+  },
+];
+
+const Navigation = () => {
+  const [view, setView] = useLocalStorage('throwaway-view', 'email');
+  return (
+    <div className='flex whitespace-nowrap border-gray-700 bg-gray-800 absolute z-10 bottom-0 w-full py-2 border-t-2 items-center justify-around px-24'>
+      {options.map((option) => (
+        <button
+          className={`inline-flex items-center h-10 px-2 py-2 -mb-px text-center  bg-transparent  sm:px-4 -px-1 ${
+            option.path === view && 'border-blue-400 border-b-2 text-blue-300'
+          } text-gray-300 whitespace-nowrap focus:outline-none`}
+          onClick={() => setView(option.path)}
+        >
+          <option.icon />
+          <span className='mx-1 text-sm sm:text-base'>{option.label}</span>
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Navigation;
