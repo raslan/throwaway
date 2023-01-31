e97e62408fc40027d685705a279ef8b88e41995f
raslan <24810123+raslan@users.noreply.github.com>
2023-01-31 22:48:21 +0200

    feat: add new background color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        app: '#0a0e14',
+      },
+    },
   },
   plugins: [],
 };
