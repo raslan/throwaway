985c0f2c282ee7a53c143327ef191cba18275caa
raslan <24810123+raslan@users.noreply.github.com>
2023-02-04 19:09:52 +0200

    feat: initialize options ui

---
+++ src/options.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Inbox from './views/inbox';
+import './index.css';
+import { Toaster } from 'react-hot-toast';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <div className='w-screen min-h-screen bg-app text-white'>
+      <Toaster />
+      <Inbox />
+    </div>
+  </React.StrictMode>
+);
