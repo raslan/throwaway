31ab2642b05a392e545b17fbfdf78a4893918053
raslan <24810123+raslan@users.noreply.github.com>
2023-02-04 19:32:08 +0200

    refactor: cleanup dropdown component

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -4,6 +4,7 @@ import { toast } from 'react-hot-toast';
 import { Email } from 'src/types';
 import { useCopyToClipboard } from 'usehooks-ts';
 import urlRegex from 'url-regex';
+import CopyIcon from './CopyIcon';
 
 export default function Dropdown({
   otp,
@@ -109,23 +110,7 @@ export default function Dropdown({
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  <svg
-                    width={24}
-                    height={24}
-                    viewBox='0 0 24 24'
-                    fill='none'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path d='M13 7H7V5H13V7Z' fill='currentColor' />
-                    <path d='M13 11H7V9H13V11Z' fill='currentColor' />
-                    <path d='M7 15H13V13H7V15Z' fill='currentColor' />
-                    <path
-                      fillRule='evenodd'
-                      clipRule='evenodd'
-                      d='M3 19V1H17V5H21V23H7V19H3ZM15 17V3H5V17H15ZM17 7V19H9V21H19V7H17Z'
-                      fill='currentColor'
-                    />
-                  </svg>
+                  <CopyIcon />
                   Open Link In Email
                 </a>
               ) : null}
