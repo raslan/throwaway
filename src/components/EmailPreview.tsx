3d2d637fad3019e17e0cf83f2b49258f5077e120
raslan <24810123+raslan@users.noreply.github.com>
2023-02-09 19:29:37 +0200

    feat: improve various aspects from beta test

--- src/components/EmailPreview.tsx
+++ src/components/EmailPreview.tsx
@@ -4,10 +4,10 @@ const EmailPreview = ({ subject, from, onClick = () => {} }: Props) => {
       onClick={onClick}
       className='flex flex-col w-full h-24 px-6 py-4 m-auto mt-4 overflow-hidden text-left bg-gray-600 border-transparent rounded-md group hover:bg-gray-700 max-h-72'
     >
-      <p className='w-screen max-w-full overflow-hidden text-base font-medium text-gray-100 whitespace-nowrap overflow-ellipsis group-hover:underline first-letter:capitalize'>
+      <p className='w-screen max-w-full truncate text-base font-medium text-gray-100 group-hover:underline first-letter:capitalize'>
         {subject}
       </p>
-      <p className='mt-3 text-sm text-gray-300 first-letter:capitalize'>
+      <p className='mt-3 text-sm truncate text-gray-300 first-letter:capitalize'>
         {from}
       </p>
     </button>
