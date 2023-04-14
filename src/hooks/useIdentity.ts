c900a266eb62db8fffa434769e2ab00d54db020c
raslan <24810123+raslan@users.noreply.github.com>
2023-04-14 02:13:07 +0200

    feat: update identity hook to use advanced mode

--- src/hooks/useIdentity.ts
+++ src/hooks/useIdentity.ts
@@ -4,68 +4,93 @@ import { new_card } from 'src/lib/generate_card.js';
 import { useLocalStorage } from 'usehooks-ts';
 import useEmail from './useEmail';
 import useSettings from './useSettings';
+import { generatePassword } from 'src/lib/utils';
+import useAdvancedMode from './useAdvancedMode';
 
 const useIdentity = (): {
   identity: Record<string, any>;
-  newIdentity: () => void;
+  newIdentity: (updateOnlyCard?: boolean) => void;
 } => {
   const { email, otp, getNewEmail } = useEmail();
   const [identity, setIdentity] = useLocalStorage<Record<string, any>>(
     'throwaway-identity',
     {}
   );
-  const { useSafeProvider } = useSettings();
+  const { useSafeProvider, activateAdvancedMode } = useSettings();
+  const { card: advancedCardMode, cardParams } = useAdvancedMode();
   const [token] = useLocalStorage<string>('throwaway-token', '');
 
-  const newIdentity = useCallback(() => {
-    const cvc = `${Math.floor(Math.random() * 899) + 100}`;
-    const card_expiry_month = `0${Math.floor(Math.random() * 8) + 1}`;
-    const card_expiry_year = `${Math.floor(Math.random() * 7) + 22}`;
-    const card_expiry = `${card_expiry_month}/${card_expiry_year}`;
-    getNewEmail();
-    const card_number = new_card();
-    const generatedIdentity = {
-      email,
-      name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-      avatar: `https://api.lorem.space/image?w=500&h=500&x=${
-        Math.floor(Math.random() * 899) + 100
-      }`,
-      card_number,
-      'cc-number': card_number,
-      cardNumber: card_number,
-      card_expiry,
-      'cc-exp': card_expiry,
-      expMonth: card_expiry_month,
-      expYear: card_expiry_year,
-      MM: card_expiry_month,
-      YY: card_expiry_year,
-      cvc,
-      card_verification: cvc,
-      'cc-csc': cvc,
-      job_title: faker.name.jobTitle(),
-      first_name: faker.name.firstName(),
-      last_name: faker.name.lastName(),
-      state: faker.address.state(),
-      city: faker.address.city(),
-      street_address: faker.address.streetAddress(),
-      street: faker.address.streetAddress(),
-      zipcode: faker.address.zipCode(),
-      phone: faker.phone.number(),
-      country: faker.address.country(),
-      company: faker.company.name(),
-      organization: faker.company.name(),
-      username: faker.internet.userName(),
-      password: faker.internet.password() + faker.internet.password(),
-      suite: faker.address.secondaryAddress(),
-      apartment: faker.address.secondaryAddress(),
-      dateofbirth: faker.date.past(50).toISOString().split('T')[0],
-      date: faker.date.past(50).toISOString().split('T')[0],
-      otp,
-      code: otp,
-      verification_code: otp,
-    };
-    setIdentity(generatedIdentity);
-  }, [otp]);
+  const newIdentity = useCallback(
+    (updateOnlyCard = false) => {
+      const cvc = `${Math.floor(Math.random() * 899) + 100}`;
+      const card_expiry_month = `0${Math.floor(Math.random() * 8) + 1}`;
+      const card_expiry_year = `${Math.floor(Math.random() * 5) + 24}`;
+      const card_expiry = `${card_expiry_month}/${card_expiry_year}`;
+      const card_number = new_card({
+        advancedMode: (activateAdvancedMode && advancedCardMode) ?? false,
+        ...cardParams,
+      });
+      if (updateOnlyCard) {
+        setIdentity({
+          ...identity,
+          card_number,
+          'cc-number': card_number,
+          cardNumber: card_number,
+          card_expiry,
+          'cc-exp': card_expiry,
+          expMonth: card_expiry_month,
+          expYear: card_expiry_year,
+          MM: card_expiry_month,
+          YY: card_expiry_year,
+          cvc,
+          card_verification: cvc,
+          'cc-csc': cvc,
+        });
+        return;
+      }
+      getNewEmail();
+      const generatedIdentity = {
+        email,
+        name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+        avatar: `https://picsum.photos/500`,
+        card_number,
+        'cc-number': card_number,
+        cardNumber: card_number,
+        card_expiry,
+        'cc-exp': card_expiry,
+        expMonth: card_expiry_month,
+        expYear: card_expiry_year,
+        MM: card_expiry_month,
+        YY: card_expiry_year,
+        cvc,
+        card_verification: cvc,
+        'cc-csc': cvc,
+        job_title: faker.name.jobTitle(),
+        first_name: faker.name.firstName(),
+        last_name: faker.name.lastName(),
+        state: faker.address.state(),
+        city: faker.address.city(),
+        street_address: faker.address.streetAddress(),
+        street: faker.address.streetAddress(),
+        zipcode: faker.address.zipCode(),
+        phone: faker.phone.number(),
+        country: faker.address.country(),
+        company: faker.company.name(),
+        organization: faker.company.name(),
+        username: faker.internet.userName(),
+        password: generatePassword(),
+        suite: faker.address.secondaryAddress(),
+        apartment: faker.address.secondaryAddress(),
+        dateofbirth: faker.date.past(50).toISOString().split('T')[0],
+        date: faker.date.past(50).toISOString().split('T')[0],
+        otp,
+        code: otp,
+        verification_code: otp,
+      };
+      setIdentity(generatedIdentity);
+    },
+    [otp, useAdvancedMode, cardParams, advancedCardMode]
+  );
 
   useEffect(() => {
     if (!identity.name) {
