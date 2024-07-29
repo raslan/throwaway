3dc37186d2846b3f8328de5f0234de62c0af283a
raslan <24810123+raslan@users.noreply.github.com>
2024-07-29 16:47:25 +0300

    feat: add generators for identity

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,19 +1,5 @@
-import { Settings } from 'src/hooks/useSettings';
-
-const SETTING_NAMES = {
-  useSafeProvider: 'Use safe email provider',
-};
-
-const SETTING_DESCRIPTIONS = {
-  useSafeProvider:
-    'Always available, turn this on if you get blank email addresses without it.',
-};
-
-export const getSettingName = (setting: keyof Settings): string =>
-  SETTING_NAMES?.[setting];
-
-export const getSettingDescription = (setting: keyof Settings): string =>
-  SETTING_DESCRIPTIONS?.[setting];
+import { faker } from '@faker-js/faker';
+import { new_card } from './generate_card';
 
 export const generatePassword = (
   length = 13,
@@ -72,4 +58,94 @@ export const generatePassword = (
   }
 
   return password;
+};
+
+export const generateUserData = (email: string) => {
+  return {
+    identifier: email,
+    avatar: `https://picsum.photos/500`,
+    job_title: faker.person.jobTitle(),
+    name: `${faker.person.firstName()} ${faker.person.lastName()}`,
+    first_name: faker.person.firstName(),
+    last_name: faker.person.lastName(),
+    phone: faker.phone.number(),
+    company: faker.company.name(),
+    organization: faker.company.name(),
+    username: faker.internet.userName(),
+    password: generatePassword(),
+  };
+};
+
+export const generateAddress = () => ({
+  state: faker.location.state(),
+  city: faker.location.city(),
+  street_address: faker.location.streetAddress(),
+  street: faker.location.streetAddress(),
+  zipcode: faker.location.zipCode(),
+  country: faker.location.country(),
+  suite: faker.location.secondaryAddress(),
+  apartment: faker.location.secondaryAddress(),
+});
+
+export const generateFinancials = () => {
+  const bic = faker.finance.bic();
+  const accountNumber = faker.finance.accountNumber();
+  const routingNumber = faker.finance.routingNumber();
+  return {
+    currency: faker.finance.currencyCode(),
+    iban: faker.finance.iban(),
+    bic,
+    swift: bic,
+    swiftCode: bic,
+    swift_code: bic,
+    account: accountNumber,
+    accountNumber,
+    account_number: accountNumber,
+    accountName: faker.finance.accountName(),
+    routingNumber,
+    routing_number: routingNumber,
+  };
+};
+
+export const generateDate = () => ({
+  dateofbirth: faker.date.past({ years: 50 }).toISOString().split('T')[0],
+  date: faker.date.past({ years: 50 }).toISOString().split('T')[0],
+});
+
+export const generateCode = (otp: string) => ({
+  otp,
+  code: otp,
+  verification_code: otp,
+});
+
+export const generateCard = (
+  advancedCardMode: boolean,
+  cardParams: {
+    provider?: 'stripe' | 'paypal' | 'amazon' | 'fawrypay' | 'paymob' | 'none';
+    brand?: 'visa' | 'mastercard';
+    variant?: 'basic' | 'debit' | 'declined' | 'expired' | 'secure';
+  }
+) => {
+  const cvc = `${Math.floor(Math.random() * 899) + 100}`;
+  const card_expiry_month = `0${Math.floor(Math.random() * 8) + 1}`;
+  const card_expiry_year = `${Math.floor(Math.random() * 5) + 24}`;
+  const card_expiry = `${card_expiry_month}/${card_expiry_year}`;
+  const card_number = new_card({
+    advancedMode: advancedCardMode ?? false,
+    ...cardParams,
+  });
+  return {
+    card_number,
+    'cc-number': card_number,
+    cardNumber: card_number,
+    card_expiry,
+    'cc-exp': card_expiry,
+    expMonth: card_expiry_month,
+    expYear: card_expiry_year,
+    MM: card_expiry_month,
+    YY: card_expiry_year,
+    cvc,
+    card_verification: cvc,
+    'cc-csc': cvc,
+  };
 };
