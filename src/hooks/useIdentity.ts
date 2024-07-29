6464554a48ed94b6c595ea806a96fd48344b8ded
raslan <24810123+raslan@users.noreply.github.com>
2024-07-29 16:46:47 +0300

    refactor: rework how identity is generated

--- src/hooks/useIdentity.ts
+++ src/hooks/useIdentity.ts
@@ -1,11 +1,15 @@
-import { faker } from '@faker-js/faker';
 import { useCallback, useEffect } from 'react';
-import { new_card } from 'src/lib/generate_card.js';
+import {
+  generateAddress,
+  generateCard,
+  generateCode,
+  generateDate,
+  generateFinancials,
+  generateUserData,
+} from 'src/lib/utils';
 import { useLocalStorage } from 'usehooks-ts';
-import useEmail from './useEmail';
-import useSettings from './useSettings';
-import { generatePassword } from 'src/lib/utils';
 import useAdvancedMode from './useAdvancedMode';
+import useEmail from './useEmail';
 
 const useIdentity = (): {
   identity: Record<string, any>;
@@ -16,79 +20,29 @@ const useIdentity = (): {
     'throwaway-identity',
     {}
   );
-  const { useSafeProvider } = useSettings();
   const { card: advancedCardMode, cardParams } = useAdvancedMode();
   const [token] = useLocalStorage<string>('throwaway-token', '');
 
   const newIdentity = useCallback(
     (updateOnlyCard = false) => {
-      const cvc = `${Math.floor(Math.random() * 899) + 100}`;
-      const card_expiry_month = `0${Math.floor(Math.random() * 8) + 1}`;
-      const card_expiry_year = `${Math.floor(Math.random() * 5) + 24}`;
-      const card_expiry = `${card_expiry_month}/${card_expiry_year}`;
-      const card_number = new_card({
-        advancedMode: advancedCardMode ?? false,
-        ...cardParams,
-      });
+      const card = generateCard(advancedCardMode, cardParams);
       if (updateOnlyCard) {
         setIdentity({
           ...identity,
-          card_number,
-          'cc-number': card_number,
-          cardNumber: card_number,
-          card_expiry,
-          'cc-exp': card_expiry,
-          expMonth: card_expiry_month,
-          expYear: card_expiry_year,
-          MM: card_expiry_month,
-          YY: card_expiry_year,
-          cvc,
-          card_verification: cvc,
-          'cc-csc': cvc,
+          ...card,
         });
         return;
       }
       getNewEmail();
-      const generatedIdentity = {
+      setIdentity({
         email,
-        identifier: email,
-        name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-        avatar: `https://picsum.photos/500`,
-        card_number,
-        'cc-number': card_number,
-        cardNumber: card_number,
-        card_expiry,
-        'cc-exp': card_expiry,
-        expMonth: card_expiry_month,
-        expYear: card_expiry_year,
-        MM: card_expiry_month,
-        YY: card_expiry_year,
-        cvc,
-        card_verification: cvc,
-        'cc-csc': cvc,
-        job_title: faker.name.jobTitle(),
-        first_name: faker.name.firstName(),
-        last_name: faker.name.lastName(),
-        state: faker.address.state(),
-        city: faker.address.city(),
-        street_address: faker.address.streetAddress(),
-        street: faker.address.streetAddress(),
-        zipcode: faker.address.zipCode(),
-        phone: faker.phone.number(),
-        country: faker.address.country(),
-        company: faker.company.name(),
-        organization: faker.company.name(),
-        username: faker.internet.userName(),
-        password: generatePassword(),
-        suite: faker.address.secondaryAddress(),
-        apartment: faker.address.secondaryAddress(),
-        dateofbirth: faker.date.past(50).toISOString().split('T')[0],
-        date: faker.date.past(50).toISOString().split('T')[0],
-        otp,
-        code: otp,
-        verification_code: otp,
-      };
-      setIdentity(generatedIdentity);
+        ...card,
+        ...generateAddress(),
+        ...generateDate(),
+        ...generateCode(otp),
+        ...generateUserData(email),
+        ...generateFinancials(),
+      });
     },
     [otp, useAdvancedMode, cardParams, advancedCardMode]
   );
@@ -106,7 +60,6 @@ const useIdentity = (): {
       identity: JSON.stringify(identity),
       throwaway_env: JSON.stringify({
         VITE_API_URL: import.meta.env.VITE_API_URL,
-        useSafeProvider,
         token,
       }),
     });
