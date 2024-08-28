328031d10e3d176c1dc0c6fe1808f5230b073a5b
raslan <24810123+raslan@users.noreply.github.com>
2024-08-28 15:44:50 +0300

    fix: handle issue with empty context menu items crashing

--- src/components/identity/CardWithControls.tsx
+++ src/components/identity/CardWithControls.tsx
@@ -16,79 +16,95 @@ const CardWithControls = ({
   copy: (text: string) => Promise<boolean>;
 }) => {
   return (
-    <ContextMenu>
-      <ContextMenuTrigger>
-        <ReactCreditCard
-          number={identity?.card_number}
-          expiry={identity?.card_expiry}
-          cvc={identity?.card_verification}
-          name='Right Click To Copy'
-          acceptedCards={['']}
-        />
-      </ContextMenuTrigger>
-      <ContextMenuContent className='w-64'>
-        <ContextMenuItem asChild>
-          <Button
-            onClick={() => {
-              copy(identity?.card_number);
-              toast.success('Copied to clipboard');
-            }}
-            variant='ghost'
-            className='w-full justify-start'
-          >
-            Copy Credit Card Number
-          </Button>
-        </ContextMenuItem>
-        <ContextMenuItem asChild>
-          <Button
-            onClick={() => {
-              copy(identity?.card_expiry);
-              toast.success('Copied to clipboard');
-            }}
-            variant='ghost'
-            className='w-full justify-start'
-          >
-            Copy Expiry Date
-          </Button>
-        </ContextMenuItem>
-        <ContextMenuItem asChild>
-          <Button
-            onClick={() => {
-              copy(identity?.card_expiry?.split?.('/')?.[0]);
-              toast.success('Copied to clipboard');
-            }}
-            variant='ghost'
-            className='w-full justify-start'
-          >
-            Copy Expiry Month
-          </Button>
-        </ContextMenuItem>
-        <ContextMenuItem asChild>
-          <Button
-            onClick={() => {
-              copy(identity?.card_expiry?.split?.('/')?.[1]);
-              toast.success('Copied to clipboard');
-            }}
-            variant='ghost'
-            className='w-full justify-start'
-          >
-            Copy Expiry Year
-          </Button>
-        </ContextMenuItem>
-        <ContextMenuItem asChild>
-          <Button
-            onClick={() => {
-              copy(identity?.card_verification);
-              toast.success('Copied to clipboard');
-            }}
-            variant='ghost'
-            className='w-full justify-start'
-          >
-            Copy CVC
-          </Button>
-        </ContextMenuItem>
-      </ContextMenuContent>
-    </ContextMenu>
+    <>
+      {identity?.card_number &&
+      identity?.card_expiry &&
+      identity.card_verification ? (
+        <ContextMenu>
+          <ContextMenuTrigger>
+            <ReactCreditCard
+              number={identity?.card_number ?? ''}
+              expiry={identity?.card_expiry ?? ''}
+              cvc={identity?.card_verification ?? ''}
+              name='Right Click To Copy'
+              acceptedCards={['']}
+            />
+          </ContextMenuTrigger>
+          <ContextMenuContent className='w-64'>
+            <ContextMenuItem asChild>
+              <Button
+                onClick={() => {
+                  if (identity?.card_number) {
+                    copy(identity?.card_number);
+                    toast.success('Copied to clipboard');
+                  }
+                }}
+                variant='ghost'
+                className='w-full justify-start'
+              >
+                Copy Credit Card Number
+              </Button>
+            </ContextMenuItem>
+            <ContextMenuItem asChild>
+              <Button
+                onClick={() => {
+                  if (identity?.card_expiry) {
+                    copy(identity?.card_expiry);
+                    toast.success('Copied to clipboard');
+                  }
+                }}
+                variant='ghost'
+                className='w-full justify-start'
+              >
+                Copy Expiry Date
+              </Button>
+            </ContextMenuItem>
+            <ContextMenuItem asChild>
+              <Button
+                onClick={() => {
+                  if (identity?.card_expiry) {
+                    copy(identity?.card_expiry?.split?.('/')?.[0]);
+                    toast.success('Copied to clipboard');
+                  }
+                }}
+                variant='ghost'
+                className='w-full justify-start'
+              >
+                Copy Expiry Month
+              </Button>
+            </ContextMenuItem>
+            <ContextMenuItem asChild>
+              <Button
+                onClick={() => {
+                  if (identity?.card_expiry) {
+                    copy(identity?.card_expiry?.split?.('/')?.[1]);
+                    toast.success('Copied to clipboard');
+                  }
+                }}
+                variant='ghost'
+                className='w-full justify-start'
+              >
+                Copy Expiry Year
+              </Button>
+            </ContextMenuItem>
+            <ContextMenuItem asChild>
+              <Button
+                onClick={() => {
+                  if (identity?.card_verification) {
+                    copy(identity?.card_verification);
+                    toast.success('Copied to clipboard');
+                  }
+                }}
+                variant='ghost'
+                className='w-full justify-start'
+              >
+                Copy CVC
+              </Button>
+            </ContextMenuItem>
+          </ContextMenuContent>
+        </ContextMenu>
+      ) : null}
+    </>
   );
 };
 
